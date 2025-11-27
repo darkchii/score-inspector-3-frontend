f8b8b6fe417a1cb800473d9bf5d6e32aefbc579a
Mark <engineermarkpro@gmail.com>
2025-11-27 23:00:12 +0100

    grades show tooltip with total grades

--- src/Components/Profile/ProfileGrades.jsx
+++ src/Components/Profile/ProfileGrades.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, Paper, SvgIcon } from "@mui/material";
+import { Box, Container, Grid, Paper, SvgIcon, Tooltip, Typography } from "@mui/material";
 import { useProfile } from "../../Providers/ProfileProvider";
 import { TextureDatabase } from "../../Data/Textures/TextureDatabase";
 import { FormatNumber } from "../../Misc/Helper";
@@ -15,49 +15,65 @@ function ProfileGrades() {
                     <Grid item size={{ xs: 12, sm: 12, md: 1.5, lg: 1.5 }}>
                         <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'start', gap: 1 }}>
                             <img src={TextureDatabase.SVGGradeXH} alt="XH" width={48} height={48} />
-                            <div>{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.XH || 0)}</div>
+                            <Tooltip title={`Including overrides: ${FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.XH || 0)} total`}>
+                                <Typography variant="h6">{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set_by_score.grades?.XH || 0)}</Typography>
+                            </Tooltip>
                         </Box>
                     </Grid>
                     <Grid item size={{ xs: 12, sm: 12, md: 1.5, lg: 1.5 }}>
                         <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'start', gap: 1 }}>
                             <img src={TextureDatabase.SVGGradeX} alt="X" width={48} height={48} />
-                            <div>{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.X || 0)}</div>
+                            <Tooltip title={`Including overrides: ${FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.X || 0)} total`}>
+                                <Typography variant="h6">{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set_by_score.grades?.X || 0)}</Typography>
+                            </Tooltip>
                         </Box>
                     </Grid>
                     <Grid item size={{ xs: 12, sm: 12, md: 1.5, lg: 1.5 }}>
                         <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'start', gap: 1 }}>
                             <img src={TextureDatabase.SVGGradeSH} alt="SH" width={48} height={48} />
-                            <div>{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.SH || 0)}</div>
+                            <Tooltip title={`Including overrides: ${FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.SH || 0)} total`}>
+                                <Typography variant="h6">{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set_by_score.grades?.SH || 0)}</Typography>
+                            </Tooltip>
                         </Box>
                     </Grid>
                     <Grid item size={{ xs: 12, sm: 12, md: 1.5, lg: 1.5 }}>
                         <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'start', gap: 1 }}>
                             <img src={TextureDatabase.SVGGradeS} alt="S" width={48} height={48} />
-                            <div>{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.S || 0)}</div>
+                            <Tooltip title={`Including overrides: ${FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.S || 0)} total`}>
+                                <Typography variant="h6">{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set_by_score.grades?.S || 0)}</Typography>
+                            </Tooltip>
                         </Box>
                     </Grid>
                     <Grid item size={{ xs: 12, sm: 12, md: 1.5, lg: 1.5 }}>
                         <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'start', gap: 1 }}>
                             <img src={TextureDatabase.SVGGradeA} alt="A" width={48} height={48} />
-                            <div>{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.A || 0)}</div>
+                            <Tooltip title={`Including overrides: ${FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.A || 0)} total`}>
+                                <Typography variant="h6">{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set_by_score.grades?.A || 0)}</Typography>
+                            </Tooltip>
                         </Box>
                     </Grid>
                     <Grid item size={{ xs: 12, sm: 12, md: 1.5, lg: 1.5 }}>
                         <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'start', gap: 1 }}>
                             <img src={TextureDatabase.SVGGradeB} alt="B" width={48} height={48} />
-                            <div>{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.B || 0)}</div>
+                            <Tooltip title={`Including overrides: ${FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.B || 0)} total`}>
+                                <Typography variant="h6">{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set_by_score.grades?.B || 0)}</Typography>
+                            </Tooltip>
                         </Box>
                     </Grid>
                     <Grid item size={{ xs: 12, sm: 12, md: 1.5, lg: 1.5 }}>
                         <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'start', gap: 1 }}>
                             <img src={TextureDatabase.SVGGradeC} alt="C" width={48} height={48} />
-                            <div>{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.C || 0)}</div>
+                            <Tooltip title={`Including overrides: ${FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.C || 0)} total`}>
+                                <Typography variant="h6">{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set_by_score.grades?.C || 0)}</Typography>
+                            </Tooltip>
                         </Box>
                     </Grid>
                     <Grid item size={{ xs: 12, sm: 12, md: 1.5, lg: 1.5 }}>
                         <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'start', gap: 1 }}>
                             <img src={TextureDatabase.SVGGradeD} alt="D" width={48} height={48} />
-                            <div>{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.D || 0)}</div>
+                            <Tooltip title={`Including overrides: ${FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set.grades?.D || 0)} total`}>
+                                <Typography variant="h6">{FormatNumber(getRulesetStatistics(activeRuleset)?.scores_set_by_score.grades?.D || 0)}</Typography>
+                            </Tooltip>
                         </Box>
                     </Grid>
                 </Grid>
