8042b25b492ba8ee1107d5683299a362ca879f3f
amayakase <engineermarkpro@gmail.com>
2025-12-05 18:09:46 +0100

    added diff badge to scoreview

---
+++ src/Components/DifficultyBadge.jsx
@@ -0,0 +1,18 @@
+import { getDiffColour } from '../Misc/DifficultyHelper';
+import { FormatNumberWithPrecision } from '../Misc/Helper';
+import difficultyBadgeStyles from '../Style/difficulty-badge.module.less';
+import StarIcon from '@mui/icons-material/Star';
+
+function DifficultyBadge({ difficulty }) {
+    return (
+        <div
+            style={{ '--bg': getDiffColour(difficulty) }}
+            className={`${difficultyBadgeStyles['difficulty-badge']} ${difficulty >= 6.5 ? difficultyBadgeStyles['difficulty-badge--expert-plus'] : ''}`}
+        >
+            <StarIcon className={difficultyBadgeStyles['difficulty-badge__icon']}/>
+            <span className={difficultyBadgeStyles['difficulty-badge__rating']}>{FormatNumberWithPrecision(difficulty, 2)}</span>
+        </div>
+    )
+}
+
+export default DifficultyBadge;
--- src/Components/ScoreView.jsx
+++ src/Components/ScoreView.jsx
@@ -1,10 +1,12 @@
 import { Box, Grid, Stack, Typography } from '@mui/material';
 import scoreModalStyles from '../Style/score-modal.module.less';
 import scoreInfoStyles from '../Style/score-info.module.less';
+import scoreStatsStyles from '../Style/score-info.module.less';
 import ScoreDial from './ScoreDial';
 import NumberFlow from '@number-flow/react';
-import { DateToString, FormatNumber } from '../Misc/Helper';
+import { DateToString, FormatNumber, GetRulesetIconFromId } from '../Misc/Helper';
 import ModDisplay from './ModDisplay';
+import DifficultyBadge from './DifficultyBadge';
 
 function ScoreView({ score }) {
 
@@ -14,6 +16,10 @@ function ScoreView({ score }) {
                 <p>user data area</p>
             </div>
             <div className={scoreModalStyles['score-modal__content']}>
+                <div className={scoreModalStyles['score-modal__background']} style={{
+                    backgroundImage: `url(https://assets.ppy.sh/beatmaps/${score.beatmap.beatmapset_id}/covers/fullsize.jpg)`,
+                }} />
+                <div className={scoreModalStyles['score-modal__background__overlay']} />
                 <Box sx={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
                     <div className={`${scoreInfoStyles['score-info']} ${scoreInfoStyles['score-info__title']}`}>
                         <span>{score.beatmap.title}</span>
@@ -30,15 +36,22 @@ function ScoreView({ score }) {
                     <div className={`${scoreInfoStyles['score-info']} ${scoreInfoStyles['score-info__subscore']}`}>
                         <span>{FormatNumber(score.classic_total_score)}</span>
                     </div>
-                    <div className={scoreInfoStyles['score-info']}>
+                    {/* row flex */}
+                    <div className={scoreInfoStyles['score-info']} style={{ display: 'flex', flexDirection: 'row', gap: '5px' }}>
+                        <DifficultyBadge difficulty={score.beatmap.stars} />
+                        <img src={GetRulesetIconFromId(score.ruleset_id)} alt="Ruleset Icon" style={{ width: '24px', height: '24px' }} />
                         <ModDisplay ruleset={score.ruleset} mods={score.mods} />
                     </div>
                     <div className={`${scoreInfoStyles['score-info']}`}>
                         <span className={scoreInfoStyles['score-info__version']}>{score.beatmap.version}</span>
-                        <span>mapped by <span style={{fontWeight: 'bold'}}>{score.beatmap.mapper || 'N/A'}</span></span>
+                        <span>mapped by <span style={{ fontWeight: 'bold' }}>{score.beatmap.mapper || 'N/A'}</span></span>
+                    </div>
+                    <div className={`${scoreInfoStyles['score-info']}`}>
+                        <div className={scoreStatsStyles['score-stats']}>
+                        </div>
                     </div>
                     <div className={`${scoreInfoStyles['score-info']} ${scoreInfoStyles['score-info__rankdate']}`}>
-                        <span>Ranked on {DateToString(score.beatmap.ranked_date)}</span>
+                        <span>Played on {DateToString(score.ended_at)}</span>
                     </div>
                 </Box>
             </div>
