51b155cdd6e534e84d68d7313440c5047ac1979b
amayakase <engineermarkpro@gmail.com>
2025-12-12 00:21:03 +0100

    added indicator to selected ruleset

--- src/Components/Profile/ProfileRulesetSelector.jsx
+++ src/Components/Profile/ProfileRulesetSelector.jsx
@@ -1,7 +1,8 @@
-import { Box, Button, Divider, IconButton, Tooltip, useTheme } from "@mui/material";
+import { Box, Button, Divider, Fade, IconButton, Tooltip, useTheme } from "@mui/material";
 import { useProfile } from "../../Providers/ProfileProvider";
 import { TextureDatabase } from "../../Assets/Textures/TextureDatabase";
 import { GetRulesetColor, GetRulesets } from "../../Misc/Helper";
+import { grey } from "@mui/material/colors";
 
 function ProfileRulesetSelector() {
     const { activeRuleset, setActiveRuleset, availableRulesets } = useProfile();
@@ -13,7 +14,26 @@ function ProfileRulesetSelector() {
                 {
                     GetRulesets().map((ruleset) => (
                         <Tooltip title={!availableRulesets.includes(ruleset.name) ? "No scores available for this ruleset" : ruleset.displayName}>
-                            <Box key={`ruleset_${ruleset.id}`}>
+                            <Box key={`ruleset_${ruleset.id}`} sx={{ position: 'relative' }}>
+                                {/* arrow below indicator, pointing down, colored with GetRulesetColor */}
+                                <Fade in={activeRuleset === ruleset.name} unmountOnExit>
+                                    <Box
+                                        sx={{
+                                            position: 'absolute',
+                                            bottom: -10,
+                                            left: '50%',
+                                            transform: 'translateX(-50%)',
+                                            width: 0,
+                                            height: 0,
+                                            borderLeft: '6px solid transparent',
+                                            borderRight: '6px solid transparent',
+                                            borderBottom: `8px solid ${GetRulesetColor(ruleset.name)[500]}`,
+                                            //glow
+                                            filter: `drop-shadow(0 0 4px ${GetRulesetColor(ruleset.name)[500]})`,
+                                        }}
+                                    />
+                                </Fade>
+                                {/* icon/button */}
                                 <IconButton
                                     onClick={() => availableRulesets.includes(ruleset.name) && setActiveRuleset(ruleset.name)}
                                     className={activeRuleset === ruleset.name ? "profile-selected-ruleset" : ""}
@@ -41,11 +61,11 @@ function ProfileRulesetSelector() {
                 <Divider orientation="vertical" flexItem sx={{ mx: 1 }} />
                 <IconButton key={'ruleset_combined'} onClick={() => setActiveRuleset('all')} className={activeRuleset === 'all' ? "profile-selected-ruleset" : ""} title="All Rulesets">
                     {/* show a combination of the four ruleset icons, show a corner of each */}
-                    <div style={{ 
-                        position: 'relative', 
-                        width: 24, 
+                    <div style={{
+                        position: 'relative',
+                        width: 24,
                         height: 24,
-                        }}>
+                    }}>
                         <img
                             src={TextureDatabase.RulesetOsuIcon}
                             alt="osu!"
@@ -60,8 +80,8 @@ function ProfileRulesetSelector() {
                             className="profile-ruleset-icon"
                             width={12}
                             height={12}
-                                style={{ position: 'absolute', top: 0, right: 0, filter: activeRuleset === 'all' ? `drop-shadow(0 0 4px ${GetRulesetColor('taiko')[500]})` : 'none', }}
-                            />
+                            style={{ position: 'absolute', top: 0, right: 0, filter: activeRuleset === 'all' ? `drop-shadow(0 0 4px ${GetRulesetColor('taiko')[500]})` : 'none', }}
+                        />
                         <img
                             src={TextureDatabase.RulesetCatchIcon}
                             alt="Catch the Beat"
