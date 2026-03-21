1aeed540a21bb467a749dbb607f75ea767924a8b
amayakase <engineermarkpro@gmail.com>
2026-03-21 21:17:06 +0100

    added new difficulty text colors

--- src/components/DifficultyBadge.tsx
+++ src/components/DifficultyBadge.tsx
@@ -1,16 +1,20 @@
-import { getDiffColour } from '../util/DifficultyHelper';
+import { getDiffColour, getDiffTextColour } from '../util/DifficultyHelper';
 import { FormatNumberWithPrecision } from '../util/Helper';
 import difficultyBadgeStyles from '../styles/difficulty-badge.module.less';
 import StarIcon from '@mui/icons-material/Star';
 
 type DifficultyBadgeStyleWithVars = React.CSSProperties & {
     '--bg'?: string;
+    '--text'?: string;
 };
 
 function DifficultyBadge({ difficulty }: { difficulty: number | null }) {
     return (
         <div
-            style={{ '--bg': getDiffColour(difficulty) } as DifficultyBadgeStyleWithVars}
+            style={{ 
+                '--bg': getDiffColour(difficulty),
+                '--text': getDiffTextColour(difficulty)
+            } as DifficultyBadgeStyleWithVars}
             className={`${difficultyBadgeStyles['difficulty-badge']} ${difficulty !== null && difficulty >= 6.5 ? difficultyBadgeStyles['difficulty-badge--expert-plus'] : ''}`}
         >
             <StarIcon className={difficultyBadgeStyles['difficulty-badge__icon']}/>
--- src/util/DifficultyHelper.tsx
+++ src/util/DifficultyHelper.tsx
@@ -6,9 +6,22 @@ const difficultyColourSpectrum = scaleLinear<string>()
     .range(['#4290FB', '#4FC0FF', '#4FFFD5', '#7CFF4F', '#F6F05C', '#FF8068', '#FF4E6F', '#C645B8', '#6563DE', '#18158E', '#000000'])
     .interpolate(interpolateRgb.gamma(2.2));
 
+const difficultyTextColourSpectrum = scaleLinear<string>()
+    .domain([9, 9.9, 10.6, 11.5, 12.4])
+    .clamp(true)
+    .range(['#F6F05C', '#FF8068', '#FF4E6F', '#C645B8', '#6563DE', '#18158E'])
+    .interpolate(interpolateRgb.gamma(2.2));
+
 export const getDiffColour = (rating: number | null): string => {
     if (rating === null) return '#AAAAAA';
     if (rating < 0.1) return '#AAAAAA';
     if (rating >= 9) return '#000000';
     return difficultyColourSpectrum(rating);
+}
+
+export const getDiffTextColour = (rating: number | null): string => {
+    if (rating === null) return '#AAAAAA';
+    if (rating < 6.5) return '#000000';
+    if (rating < 9) return '#F6F05C';
+    return difficultyTextColourSpectrum(rating);
 }
